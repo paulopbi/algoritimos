a51932b69252f9e65a363351a948d96a89ce4928
Paulo Victor <paulpbi16@gmail.com>
2022-12-09 21:12:38 -0300

    feat: elabore um algoritimo que leia um numero inteiro e retorne o sucessor e antecessor

---
+++ javascript/antecessor_sucessor.js
@@ -0,0 +1,4 @@
+/**Elabore um algoritimo que leia um numero inteiro e retorne o sucessor e antecessor */
+const numero = 4;
+
+console.log(`o sucessor e: ${numero + 1}, o antecessor e ${numero - 1}`);
