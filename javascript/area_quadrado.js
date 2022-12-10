10509eed121118c584ea7f707170f8cf05cbcf8d
Paulo Victor <paulpbi16@gmail.com>
2022-12-09 21:14:44 -0300

    feat: escreva um algoritimo que calcule o valor da area ao quadrado

---
+++ javascript/area_quadrado.js
@@ -0,0 +1,7 @@
+// escreva um algoritimo que calcule o valor da area ao quadrado
+
+function areaQuadrado(lado) {
+  return `O valor do quadrado e ${lado * lado}`;
+}
+
+console.log(areaQuadrado(4));
