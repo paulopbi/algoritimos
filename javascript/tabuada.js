83a1ccb0997e0283701fb38d7715723bf98a315b
Paulo Victor <paulpbi16@gmail.com>
2022-12-09 21:11:28 -0300

    feat: algoritimo de tabuada finalizado

---
+++ javascript/tabuada.js
@@ -0,0 +1,10 @@
+/** faca um algoritimo de tabuada */
+
+function tabuada(parcela) {
+  console.log(`Algoritimo De Tabuada - Criado Por Paulo Victor.`);
+  for (let x = 0; x <= 10; x++) {
+    console.log(`${parcela} x ${x} = ${parcela * x}`);
+  }
+}
+
+tabuada(4);
